d68f4aad48fafaa479339f83715ed9aa2ecdb12b
Daniel Danielecki <daniel.danielecki@mail.com>
2020-07-16 20:45:28 +0200

    Add: 1 TODO/skip-ci

--- libs/contact/src/lib/contact.module.ts
+++ libs/contact/src/lib/contact.module.ts
@@ -1,20 +1,20 @@
-import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { AngularFireStorageModule } from '@angular/fire/storage';
-import { ContactComponent } from './contact/contact.component';
-import { environment } from '@apps/ditectrev/src/environments/environment';
-import { MaterialFileInputModule } from 'ngx-material-file-input';
-import { Ng2TelInputModule } from 'ng2-tel-input';
-import { NgModule } from '@angular/core';
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { AngularFireStorageModule } from "@angular/fire/storage";
+import { ContactComponent } from "./contact/contact.component";
+import { environment } from "@apps/ditectrev/src/environments/environment";
+import { MaterialFileInputModule } from "ngx-material-file-input";
+import { Ng2TelInputModule } from "ng2-tel-input";
+import { NgModule } from "@angular/core";
 import {
   RecaptchaModule,
   RECAPTCHA_SETTINGS,
-  RecaptchaSettings
-} from 'ng-recaptcha';
-import { RecaptchaFormsModule } from 'ng-recaptcha/forms';
-import { RouterModule } from '@angular/router';
+  RecaptchaSettings,
+} from "ng-recaptcha";
+import { RecaptchaFormsModule } from "ng-recaptcha/forms";
+import { RouterModule } from "@angular/router";
 // tslint:disable-next-line:nx-enforce-module-boundaries
-import { SharedModule } from './../../../../libs/shared/src/index';
+import { SharedModule } from "./../../../../libs/shared/src/index";
 
 @NgModule({
   declarations: [ContactComponent],
@@ -26,16 +26,16 @@ import { SharedModule } from './../../../../libs/shared/src/index';
     Ng2TelInputModule,
     RecaptchaModule,
     RecaptchaFormsModule,
-    RouterModule.forChild([{ path: '', component: ContactComponent }]),
-    SharedModule
+    RouterModule.forChild([{ path: "", component: ContactComponent }]),
+    SharedModule,
   ],
   providers: [
     {
-      provide: RECAPTCHA_SETTINGS,
+      provide: RECAPTCHA_SETTINGS, // TODO: Try reCAPTCHA v3.
       useValue: {
-        siteKey: String(process.env.RECAPTCHA_API_KEY) // TODO: It causes problem when it goes from GitLab CI.
-      } as RecaptchaSettings
-    }
-  ]
+        siteKey: String(process.env.RECAPTCHA_API_KEY), // TODO: It causes problem when it goes from GitLab CI.
+      } as RecaptchaSettings,
+    },
+  ],
 })
 export class ContactModule {}
