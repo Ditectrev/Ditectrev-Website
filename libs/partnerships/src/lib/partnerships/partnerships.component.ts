00210e08e992d01b4977a63cf45bb5a6e9d3b21d
Daniel Danielecki <daniel.danielecki@mail.com>
2020-10-26 20:15:25 +0100

    Refactor: introduce ngFor in Partnerships/skip-ci

--- libs/partnerships/src/lib/partnerships/partnerships.component.ts
+++ libs/partnerships/src/lib/partnerships/partnerships.component.ts
@@ -1,8 +1,39 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
+/**
+ * @component PartnershipsComponent
+ * @description Create the component.
+ */
 @Component({
-  selector: "app-partnerships",
-  templateUrl: "./partnerships.component.html",
-  styleUrls: ["./partnerships.component.scss"],
+  selector: 'app-partnerships',
+  templateUrl: './partnerships.component.html',
+  styleUrls: ['./partnerships.component.scss'],
 })
-export class PartnershipsComponent {}
+export class PartnershipsComponent {
+  // Array to hold all Cyber Security services.
+  // TODO: Make this interface.
+  public partnershipsItems: {
+    description: string;
+    icon: string;
+    name: string;
+  }[] = [
+    {
+      description:
+        'Sample content goes here... Sample content goes here... Sample content goes here... Sample content goes here...',
+      icon: 'grade',
+      name: 'Creative Agencies',
+    },
+    {
+      description:
+        'Sample content goes here... Sample content goes here... Sample content goes here... Sample content goes here...',
+      icon: 'perm_identity',
+      name: 'Freelancers',
+    },
+    {
+      description:
+        'Sample content goes here... Sample content goes here... Sample content goes here... Sample content goes here...',
+      icon: 'code',
+      name: 'Software Houses',
+    },
+  ];
+}
