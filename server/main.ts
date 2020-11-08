2b4cbe280c1b33943eedcbdf4a314a456d6b5338
Daniel Danielecki <daniel.danielecki@mail.com>
2020-11-08 17:11:11 +0100

    Add: uncomment Security Headers commented out code/skip-ci

--- server/main.ts
+++ server/main.ts
@@ -1,16 +1,16 @@
 // TODO: Check handling reporting violations (helmet) & bugs (sentry).
 // TODO: Add subresourceIntegrity (from Angular CLI).
 // TODO: Add nonce-based CSP.
-// TODO: Uncomment the code after upgrade to Angular 10.
+// TODO: Improve on TypeScript typings.
 import 'zone.js/dist/zone-node'; // Without that the app breaks on SSR.
 import * as admin from 'firebase-admin';
-// import * as csurf from "csurf";
+import * as csurf from 'csurf';
 import * as express from 'express';
 import * as functions from 'firebase-functions';
 import * as logger from 'morgan';
 import * as nodemailer from 'nodemailer';
-// import * as rateLimit from "express-rate-limit";
-// import * as session from "express-session";
+import * as rateLimit from 'express-rate-limit';
+import * as session from 'express-session';
 import { ApplicationModule } from './app.module';
 import { enableProdMode } from '@angular/core';
 import { Express } from 'express';
@@ -19,6 +19,7 @@ import {
   NestExpressApplication,
 } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
+import { RateLimit } from 'express-rate-limit';
 
 const helmet = require('helmet');
 
@@ -52,75 +53,75 @@ async function bootstrap() {
     })
   );
 
-  // expressApp.use(
-  //   helmet.contentSecurityPolicy({
-  //     directives: {
-  //       baseUri: ["'self'"], // Restricts use of the "<base>" tag to origin (without subdomains). This directive doesn't use "default-src" as fallback, thus by default it allows anything.
-  //       blockAllMixedContent: true, // Prevent loading any assets using HTTP when the page is loaded using HTTPS.
-  //       childSrc: [
-  //         "'self'", // Default policy for valid sources for web workers and nested browsing contexts loaded using elements such as "<frame>" and "<iframe>": allow all content coming from origin (without subdomains).
-  //         "https://vars.hotjar.com", // Hotjar.
-  //       ],
-  //       connectSrc: [
-  //         "'self'", // Default policy for restricting the URLs which can be loaded using script interfaces: allow all content coming from origin (without subdomains).
-  //         "https://agastya-version.oswaldlabs.com", // Agastya.
-  //         "https://firebasestorage.googleapis.com", // Cloud Storage for Firebase.
-  //         "https://firestore.googleapis.com", // Cloud Firestore.
-  //         "https://platform-beta.oswaldlabs.com", // Agastya.
-  //         "https://www.google-analytics.com", // Universal Analytics (Google Analytics).
-  //         "https://*.hotjar.com:*", // Hotjar.
-  //         "https://vc.hotjar.io:*", // Hotjar.
-  //         "wss://*.hotjar.com", // Hotjar.
-  //       ],
-  //       defaultSrc: [
-  //         "'none'", // Default policy for fallback for the other CSP fetch directives [Link of these: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/default-src]: disallows everything.
-  //       ],
-  //       fontSrc: [
-  //         "'self'", // Default policy for specifiying valid sources for fonts loaded using "@font-face": allow all content coming from origin (without subdomains).
-  //         "https://fonts.gstatic.com", // Google Fonts.
-  //         "https://script.hotjar.com", // Hotjar.
-  //       ],
-  //       formAction: ["'self'"], // Default policy for restricting the URLs which can be used as the target of a form submissions from a given context: allow all content coming from origin (without subdomains). This directive doesn't use "default-src" as fallback, thus by default it allows anything.
-  //       frameAncestors: ["'self'"], // Default policy for specyfing valid parents that may embed a page using "<frame>", "<iframe>", "<object>", "<embed>", or "<applet>". This directive doesn't use "default-src" as fallback, thus by default it allows anything. This is basically clickjacking protection.
-  //       frameSrc: [
-  //         "'self'", // Default policy for specyfing valid sources for nested browsing contexts loading using elements such as "<frame>" and "<iframe>": allow all content coming from origin (without subdomains).
-  //         "https://agastya-version.oswaldlabs.com", // Agastya.
-  //         "https://vars.hotjar.com", // Hotjar.
-  //         "https://www.google.com", // reCAPTCHA.
-  //       ],
-  //       imgSrc: [
-  //         "'self'", // Default policy for specyfing valid sources of images and favicons: allow all content coming from origin (without subdomains).
-  //         "https://www.google-analytics.com", // Universal Analytics (Google Analytics).
-  //         "https://www.googletagmanager.com", // Google Tag Manager.
-  //         "https://www.google.com", // reCAPTCHA.
-  //         "https://script.hotjar.com", // Hotjar.
-  //       ],
-  //       manifestSrc: ["'self'"], // Default policy for specyfing which manifest can be applied to the resource: allow all content coming from origin (without subdomains).
-  //       objectSrc: ["'none'"], // Default policy for specyfing valid sources for the "<object>", "<embed>", and "<applet>" elements. It also influences "pluginType" by disallowing all of them. The "pluginType" directive doesn't use "default-src" as fallback, thus by default it allows anything.
-  //       scriptSrc: [
-  //         "'self'", // Default policy for valid sources for JavaScript: allow all content coming from origin (without subdomains).
-  //         "'unsafe-eval'", // Unsecure, but required due to Angular's SSR.
-  //         "https://agastya-version.oswaldlabs.com", // Agastya.
-  //         "https://ditectrev.us15.list-manage.com", // MailChimp.
-  //         "https://platform.oswaldlabs.com", // Agastya.
-  //         "https://platform-beta.oswaldlabs.com", // Agastya.
-  //         "https://script.hotjar.com", // Hotjar.
-  //         "https://static.hotjar.com", // Hotjar.
-  //         "https://ssl.google-analytics.com", // Universal Analytics (Google Analytics).
-  //         "https://www.google-analytics.com", // Universal Analytics (Google Analytics).
-  //         "https://www.googletagmanager.com", // Google Tag Manager.
-  //         "https://www.google.com", // reCAPTCHA.
-  //         "https://www.gstatic.com", // reCAPTCHA.
-  //       ],
-  //       styleSrc: [
-  //         "'self'", // Default policy for valid sources for stylesheets: allow all content coming from origin (without subdomains).
-  //         "'unsafe-inline'", // Unsecure, but required in order to render styles generated by Angular compiler, which on SSR are generated as inline styles.
-  //         "https://fonts.googleapis.com", // Google Fonts.
-  //       ],
-  //       upgradeInsecureRequests: true, // Block loading of active/passive content over insecure FTP/HTTP by "upgrading" the connection to secure SFTP/HTTPS.
-  //     },
-  //   })
-  // );
+  expressApp.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        baseUri: ["'self'"], // Restricts use of the "<base>" tag to origin (without subdomains). This directive doesn't use "default-src" as fallback, thus by default it allows anything.
+        blockAllMixedContent: true, // Prevent loading any assets using HTTP when the page is loaded using HTTPS.
+        childSrc: [
+          "'self'", // Default policy for valid sources for web workers and nested browsing contexts loaded using elements such as "<frame>" and "<iframe>": allow all content coming from origin (without subdomains).
+          'https://vars.hotjar.com', // Hotjar.
+        ],
+        connectSrc: [
+          "'self'", // Default policy for restricting the URLs which can be loaded using script interfaces: allow all content coming from origin (without subdomains).
+          'https://agastya-version.oswaldlabs.com', // Agastya.
+          'https://firebasestorage.googleapis.com', // Cloud Storage for Firebase.
+          'https://firestore.googleapis.com', // Cloud Firestore.
+          'https://platform-beta.oswaldlabs.com', // Agastya.
+          'https://www.google-analytics.com', // Universal Analytics (Google Analytics).
+          'https://*.hotjar.com:*', // Hotjar.
+          'https://vc.hotjar.io:*', // Hotjar.
+          'wss://*.hotjar.com', // Hotjar.
+        ],
+        defaultSrc: [
+          "'none'", // Default policy for fallback for the other CSP fetch directives [Link of these: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/default-src]: disallows everything.
+        ],
+        fontSrc: [
+          "'self'", // Default policy for specifiying valid sources for fonts loaded using "@font-face": allow all content coming from origin (without subdomains).
+          'https://fonts.gstatic.com', // Google Fonts.
+          'https://script.hotjar.com', // Hotjar.
+        ],
+        formAction: ["'self'"], // Default policy for restricting the URLs which can be used as the target of a form submissions from a given context: allow all content coming from origin (without subdomains). This directive doesn't use "default-src" as fallback, thus by default it allows anything.
+        frameAncestors: ["'self'"], // Default policy for specyfing valid parents that may embed a page using "<frame>", "<iframe>", "<object>", "<embed>", or "<applet>". This directive doesn't use "default-src" as fallback, thus by default it allows anything. This is basically clickjacking protection.
+        frameSrc: [
+          "'self'", // Default policy for specyfing valid sources for nested browsing contexts loading using elements such as "<frame>" and "<iframe>": allow all content coming from origin (without subdomains).
+          'https://agastya-version.oswaldlabs.com', // Agastya.
+          'https://vars.hotjar.com', // Hotjar.
+          'https://www.google.com', // reCAPTCHA.
+        ],
+        imgSrc: [
+          "'self'", // Default policy for specyfing valid sources of images and favicons: allow all content coming from origin (without subdomains).
+          'https://www.google-analytics.com', // Universal Analytics (Google Analytics).
+          'https://www.googletagmanager.com', // Google Tag Manager.
+          'https://www.google.com', // reCAPTCHA.
+          'https://script.hotjar.com', // Hotjar.
+        ],
+        manifestSrc: ["'self'"], // Default policy for specyfing which manifest can be applied to the resource: allow all content coming from origin (without subdomains).
+        objectSrc: ["'none'"], // Default policy for specyfing valid sources for the "<object>", "<embed>", and "<applet>" elements. It also influences "pluginType" by disallowing all of them. The "pluginType" directive doesn't use "default-src" as fallback, thus by default it allows anything.
+        scriptSrc: [
+          "'self'", // Default policy for valid sources for JavaScript: allow all content coming from origin (without subdomains).
+          "'unsafe-eval'", // Unsecure, but required due to Angular's SSR.
+          'https://agastya-version.oswaldlabs.com', // Agastya.
+          'https://ditectrev.us15.list-manage.com', // MailChimp.
+          'https://platform.oswaldlabs.com', // Agastya.
+          'https://platform-beta.oswaldlabs.com', // Agastya.
+          'https://script.hotjar.com', // Hotjar.
+          'https://static.hotjar.com', // Hotjar.
+          'https://ssl.google-analytics.com', // Universal Analytics (Google Analytics).
+          'https://www.google-analytics.com', // Universal Analytics (Google Analytics).
+          'https://www.googletagmanager.com', // Google Tag Manager.
+          'https://www.google.com', // reCAPTCHA.
+          'https://www.gstatic.com', // reCAPTCHA.
+        ],
+        styleSrc: [
+          "'self'", // Default policy for valid sources for stylesheets: allow all content coming from origin (without subdomains).
+          "'unsafe-inline'", // Unsecure, but required in order to render styles generated by Angular compiler, which on SSR are generated as inline styles.
+          'https://fonts.googleapis.com', // Google Fonts.
+        ],
+        upgradeInsecureRequests: true, // Block loading of active/passive content over insecure FTP/HTTP by "upgrading" the connection to secure SFTP/HTTPS.
+      },
+    })
+  );
 
   expressApp.use(helmet.permittedCrossDomainPolicies()); // Prevent Adobe Flash and Adobe Acrobat from loading content.
 
@@ -133,51 +134,47 @@ async function bootstrap() {
   );
 
   // Limit website features by implementing Feature Policy.
-  // expressApp.use(
-  //   helmet.featurePolicy({
-  //     features: {
-  //       fullscreen: ["'self'"],
-  //       payment: ["'none'"],
-  //       syncXhr: ["'none'"],
-  //     },
-  //   })
-  // );
+  expressApp.use(
+    helmet.featurePolicy({
+      features: {
+        fullscreen: ["'self'"],
+        payment: ["'none'"],
+        syncXhr: ["'none'"],
+      },
+    })
+  );
 
-  // expressApp.use(helmet.noCache()); // Disable client-side caching.
-  // expressApp.use(helmet.referrerPolicy({ policy: "same-origin" })); // Send Referer header only for pages on the same origin.
+  expressApp.use(helmet.noCache()); // Disable client-side caching.
+  expressApp.use(helmet.referrerPolicy({ policy: 'same-origin' })); // Send Referer header only for pages on the same origin.
 
   // Handle HTTP POST request and expose it on "req.body".
   expressApp.use(express.json());
   expressApp.use(express.urlencoded({ extended: false })); // Accept only arrays or strings.
 
-  // Improve sessions and cookies security.
-  // expressApp.use(
-  //   session({
-  //     cookie: {
-  //       maxAge: 3600000, // In milliseconds, keep the session for maximum 1 hour and later expire it. Cookie by default (in the csrf package) has the same expiration date.
-  //       secure: true, // Enforce cookies has to be transmitted only through HTTPS. It prevents cookie from being transmitted through insecure HTTP.
-  //       sameSite: true, // Mitigate risk of cross-origin information leakage. Robust refence against CSRF, mitigate XSSI too.
-  //       signed: true, // Sign a cookie to prevent from cookie forging.
-  //     },
-  //     name: "SESSION_ID", // Change default name of session cookie which reveals application's internal technology. For Express apps this is "connect.sid".
-  //     resave: false, // Disable forcing session to be saved back to the sessions store, even if the session was never modified during the request. Enabling it could potentially create race conditions where client makes 2 parallels requests to the server.
-  //     saveUninitialized: true, // Save uninitialized session to the store.
-  //     secret: String(process.env.SESSION_SECRET), // Make sure the environmental variable is a string.
-  //   })
-  // );
+  const rateLimiter: RateLimit | any = rateLimit({
+    max: 100, // Max 100 connections per windowMs can be done before sending HTTP 429 (Too Many Requests) response code. After 100 requests within 15 minutes block the IP.
+    message:
+      'This IP has been temporarily blocked due to too many requests, please try again later.',
+    windowMs: 15 * 60 * 1000, // In milliseconds, keep records of requests in memory for 15 minutes.
+  });
 
-  // expressApp.use(csurf()); // Enable CSRF protection. Must be after sessions and cookies security.
+  const sessionSettings: any = session({
+    cookie: {
+      maxAge: 3600000, // In milliseconds, keep the session for maximum 1 hour and later expire it. Cookie by default (in the csrf package) has the same expiration date.
+      secure: true, // Enforce cookies has to be transmitted only through HTTPS. It prevents cookie from being transmitted through insecure HTTP.
+      sameSite: true, // Mitigate risk of cross-origin information leakage. Robust refence against CSRF, mitigate XSSI too.
+      signed: true, // Sign a cookie to prevent from cookie forging.
+    },
+    name: 'SESSION_ID', // Change default name of session cookie which reveals application's internal technology. For Express apps this is "connect.sid".
+    resave: false, // Disable forcing session to be saved back to the sessions store, even if the session was never modified during the request. Enabling it could potentially create race conditions where client makes 2 parallels requests to the server.
+    saveUninitialized: true, // Save uninitialized session to the store.
+    secret: String(process.env.SESSION_SECRET), // Make sure the environmental variable is a string.
+  });
 
+  expressApp.use(sessionSettings); // Improve sessions and cookies security.
+  expressApp.use(csurf()); // Enable CSRF protection. Must be after sessions and cookies security.
   expressApp.set('trust proxy', 1); // Trust first proxy. Enable because the application is behind reverse proxy (Firebase). For Node.js applications behind proxy it is required to enable it.
-
-  // expressApp.use(
-  // new rateLimit({
-  //   max: 100, // Max 100 connections per windowMs can be done before sending HTTP 429 (Too Many Requests) response code. After 100 requests within 15 minutes block the IP.
-  //   message:
-  //     "This IP has been temporarily blocked due to too many requests, please try again later.",
-  //   windowMs: 15 * 60 * 1000, // In milliseconds, keep records of requests in memory for 15 minutes.
-  // })
-  // );
+  expressApp.use(rateLimiter); // Apply to all requests.
 
   const nestApp = await NestFactory.create<NestExpressApplication>(
     ApplicationModule,
@@ -193,11 +190,11 @@ async function bootstrap() {
 
   nestApp.init();
   // await nestApp.listen(4479); // Initialize NestJS server, and listen on specific port (for testing on localhost). Use when testing locally without Firebase Cloud Functions solely on NestJS.
-} //)().catch((err) => console.error(err));
+}
 
 // TODO: Add ".region('europe-west')" all Firebase Cloud Functions, issue #842.
 // Firebase Cloud Function for Server Side Rendering (SSR).
-exports.angularUniversalFunction = functions.https.onRequest(<any>expressApp); // TODO: Remove the "<any>", issue #587.
+exports.angularUniversalFunction = functions.https.onRequest(<any>expressApp);
 
 // Firebase Cloud Function for sending e-mail from a contact form.
 exports.contactFormFunction = functions.firestore
